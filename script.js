5a7593a3a4578fa997413dd22d79d99399781c2f
xorgzz <118397053+xorgzz@users.noreply.github.com>
2022-11-20 19:22:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,286 @@
+let sets = [[["absent-minded"],["roztargniony"]],
+[["adventurous"],["śmiały","zuchwały","żądny przygód"]],
+[["affectionate"],["serdeczny","czuły"]],
+[["aggressive"],["agresywny"]],
+[["alert"],["czujny","bystry"]],
+[["ambitious"],["ambitny"]],
+[["amusing"],["zabawny"]],
+[["arrogant"],["arogancki"]],
+[["articulate"],["jasno wysławiający się","elokwentny"]],
+[["assertive"],["asertywny"]],
+[["bad-tempered"],["wybuchowy"]],
+[["bashful"],["wstydliwy"]],
+[["big-headed"],["zarozumiały"]],
+[["bigoted"],["dogmatyczny","bigoteryjny"]],
+[["blunt"],["szczery do bólu","otwarty"]],
+[["boastful"],["chełpliwy","przechwalający się"]],
+[["bold"],["odważny","zuchwały"]],
+[["bossy"],["władczy","apodyktyczny"]],
+[["brave"],["odważny"]],
+[["bright"],["bystry","radosny"]],
+[["broad-minded"],["tolerancyjny","o otwartym umyśle"]],
+[["brutal"],["brutalny"]],
+[["callous"],["bezduszny","bezwzględny"]],
+[["calm"],["spokojny"]],
+[["candid"],["szczery"]],
+[["carefree"],["beztroski"]],
+[["caring"],["troskliwy"]],
+[["cautious"],["ostrożny"]],
+[["changeable"],["zmienny"]],
+[["charismatic"],["charyzmatyczny"]],
+[["cheeky"],["bezczelny"]],
+[["cheerful"],["pogodny","radosny"]],
+[["cocky"],["pewny siebie","chojrak"]],
+[["competitive"],["lubiący rywalizować"]],
+[["conceited"],["zarozumiały","przemądrzały"]],
+[["conscientious"],["sumienny","skrupulatny"]],
+[["considerate"],["taktowny","liczący się z innymi"]],
+[["courageous"],["odważny"]],
+[["cowardly"],["tchórzliwy"]],
+[["creative"],["kreatywny"]],
+[["critical"],["krytyczny"]],
+[["cruel"],["okrutny"]],
+[["curious"],["ciekawy"]],
+[["cynical"],["cyniczny"]],
+[["dedicated"],["oddany"]],
+[["demanding"],["wymagający"]],
+[["dependable"],["godny zaufania / niezawodny"]],
+[["dependent"],["zależny"]],
+[["depressed"],["przygnębiony"]],
+[["determined"],["zdeterminowany"]],
+[["devoted"],["oddany","wierny"]],
+[["dishonest"],["nieuczciwy"]],
+[["disorganized"],["zdezorganizowany"]],
+[["dominant","domineering"],["dominujący"]],
+[["down-to-earth"],["praktyczny","twardo stąpa po ziemi"]],
+[["easy-going"],["wyrozumiały / mało wymagający / spokojny"]],
+[["eccentric"],["ekscentryczny","dziwaczny"]],
+[["efficient"],["sprawny / kompetentny / efektywny"]],
+[["egocentric"],["egocentryczny"]],
+[["emotional"],["emocjonalny","uczuciowy"]],
+[["energetic"],["energiczny"]],
+[["enthusiastic"],["entuzjastyczny"]],
+[["excitable"],["pobudliwy"]],
+[["extravagant"],["ekstrawagancki"]],
+[["extrovert"],["ekstrawertywny"]],
+[["fair-minded"],["bezstronny"]],
+[["fanatical"],["fanatyczny"]],
+[["flexible"],["elastyczny"]],
+[["focused"],["dążący do celu","skupiony"]],
+[["forgetful"],["zapominalski"]],
+[["forgiving"],["wielkoduszny"]],
+[["frank"],["szczery"]],
+[["frustrated"],["sfrustrowany"]],
+[["fun-loving"],["lubiący się bawić"]],
+[["funny"],["śmieszny","zabawny"]],
+[["fussy"],["wybredny","grymaśny"]],
+[["generous"],["hojny"]],
+[["gentle"],["łagodny","delikatny"]],
+[["good-tempered"],["łagodny"]],
+[["gullible"],["naiwny","łatwowierny"]],
+[["helpful"],["pomocny"]],
+[["highly-strung"],["bardzo nerwowy"]],
+[["hospitable"],["gościnny"]],
+[["hot-blooded"],["porywczy"]],
+[["humble"],["skromny","pokorny"]],
+[["hypocritical"],["zakłamany"]],
+[["illiterate"],["niepiśmienny"]],
+[["illogical"],["nielogiczny"]],
+[["imaginative"],["obdarzony wyobraźnią"]],
+[["immature"],["niedojrzały"]],
+[["impatient"],["niecierpliwy"]],
+[["impetuous"],["porywczy","impulsywny"]],
+[["impressionable"],["podatny na wpływy"]],
+[["impulsive"],["impulsywny"]],
+[["indecisive"],["niezdecydowany"]],
+[["independent"],["niezależny"]],
+[["individualistic"],["indywidualistyczny","oryginalny"]],
+[["ingenious"],["pomysłowy"]],
+[["insecure"],["niepewny"]],
+[["insensitive"],["niewrażliwy"]],
+[["insincere"],["nieszczery"]],
+[["intelligent"],["inteligentny"]],
+[["intolerant"],["nietolerancyjny"]],
+[["introvert"],["zamknięty w sobie"]],
+[["inventive"],["pomysłowy"]],
+[["ironic"],["ironiczny"]],
+[["irresponsible"],["nieodpowiedzialny"]],
+[["laid-back"],["wyluzowany"]],
+[["level-headed"],["trzeźwo myślący"]],
+[["light-hearted"],["radosny","beztroski"]],
+[["likeable"],["sympatyczny","miły"]],
+[["lively"],["pełen życia"]],
+[["logical"],["logiczny"]],
+[["loyal"],["lojalny"]],
+[["malicious"],["złośliwy"]],
+[["materialistic"],["materialistyczny"]],
+[["mature"],["dojrzały"]],
+[["mean"],["skąpy / podły"]],
+[["mercenary"],["wyrachowany"]],
+[["modest"],["skromny"]],
+[["moody"],["kapryśny","humorzasty"]],
+[["naive"],["naiwny"]],
+[["narrow-minded"],["ograniczony","nietolerancyjny"]],
+[["neurotic"],["przewrażliwiony","nerwicowy"]],
+[["obedient"],["posłuszny"]],
+[["obsessive"],["obsesyjny"]],
+[["obstinate"],["uparty"]],
+[["open-minded"],["otwarty","pozbawiony uprzedzeń"]],
+[["optimistic"],["optymistyczny"]],
+[["organized"],["zorganizowany"]],
+[["outgoing"],["towarzyski"]],
+[["overbearing"],["apodyktyczny"]],
+[["passionate"],["pełen pasji"]],
+[["passive"],["pasywny"]],
+[["peaceful"],["spokojny"]],
+[["pessimistic"],["pesymistyczny"]],
+[["petty"],["małostkowy"]],
+[["pompous"],["pompatyczny","napuszony"]],
+[["possessive"],["zaborczy"]],
+[["practical"],["praktyczny"]],
+[["prejudiced"],["uprzedzony"]],
+[["presentable"],["porządny","dobrze się prezentujący"]],
+[["protective"],["opiekuńczy"]],
+[["proud"],["dumny"]],
+[["pushy"],["bezczelny","rozpychający się łokciami"]],
+[["quiet"],["cichy"]],
+[["rash"],["pochopny","lekkomyślny"]],
+[["rational"],["racjonalny","rozsądny"]],
+[["realistic"],["realistyczny"]],
+[["rebellious"],["buntowniczy"]],
+[["relaxed"],["zrelaksowany"]],
+[["reliable"],["niezawodny","można na nim polegać"]],
+[["reserved"],["skryty","zamknięty w sobie"]],
+[["resourceful"],["przedsiębiorczy","zaradny"]],
+[["respectable"],["szanowany"]],
+[["responsible"],["odpowiedzialny"]],
+[["revolutionary"],["rewolucyjny"]],
+[["ruthless"],["bezwzględny","bezlitosny"]],
+[["sarcastic"],["sarkastyczny"]],
+[["secretive"],["skryty","mający tajemnice"]],
+[["self-assured"],["pewny siebie"]],
+[["self-centered", "self-centred"],["egocentryczny"]],
+[["self-confident"],["pewny siebie"]],
+[["self-conscious"],["nieśmiały","skrępowany","samokrytyczny"]],
+[["self-critical"],["samokrytyczny"]],
+[["selfish"],["samolubny"]],
+[["self-sufficient"],["samowystarczalny"]],
+[["sensible"],["rozsądny"]],
+[["sensitive"],["wrażliwy"]],
+[["shrewd"],["bystry","sprytny"]],
+[["shy"],["nieśmiały"]],
+[["single-minded"],["zdeterminowany"]],
+[["smug"],["zadowolony z siebie"]],
+[["snobbish"],["snobistyczny"]],
+[["sociable"],["towarzyski"]],
+[["soft-hearted"],["gołebiego serca","pobłażliwy"]],
+[["spiteful"],["złośliwy","mściwy"]],
+[["spontaneous"],["spontaniczny"]],
+[["stable"],["stabilny","zrównoważony"]],
+[["steady"],["solidny"]],
+[["straightforward"],["prostolinijny","bezpośredni"]],
+[["strict"],["surowy"]],
+[["strong-willed"],["uparty (dwa-słowa)"]],
+[["stubborn"],["uparty"]],
+[["superstitious"],["przesądny"]],
+[["sympathetic"],["pełen współczucia / życzliwy"]],
+[["tactful"],["taktowny"]],
+[["talented"],["utalentowany"]],
+[["talkative"],["rozmowny","gadatliwy"]],
+[["temperamental"],["wybuchowy"]],
+[["tender"],["czuły"]],
+[["thorough"],["skrupulatny","dokładny"]],
+[["thoughtful"],["zamyślony","życzliwy","myślący o innych"]],
+[["thrifty"],["gospodarny","oszczędny"]],
+[["tight-fisted"],["skąpy"]],
+[["timid"],["nieśmiały"]],
+[["tolerant"],["tolerancyjny"]],
+[["tough-minded"],["twardy","bezkompromisowy"]],
+[["truthful"],["prawdomówny"]],
+[["two-faced"],["dwulicowy"]],
+[["uncontrollable"],["nieokiełznany","niepohamowany"]],
+[["understanding"],["wyrozumiały"]],
+[["unpredictable"],["nieprzewidywalny"]],
+[["unreliable"],["niesolidny","zawodny"]],
+[["unscrupulous"],["pozbawiony skrupułów"]],
+[["unstable"],["niestabilny"]],
+[["vain"],["próżny"]],
+[["versatile"],["wszechstronny"]],
+[["vicious"],["bezwzględny","brutalny","bestialski"]],
+[["vindictive"],["mściwy"]],
+[["violent"],["agresywny","gwałtowny"]],
+[["warm"],["ciepły"]],
+[["weak-willed"],["mający słaby charakter"]],
+[["well-balanced"],["zrównoważony"]],
+[["well-mannered"],["dobrze-wychowany"]],
+[["wise"],["mądry"]],
+[["witty"],["dowcipny"]]];
+let done = [];
+
+let wranswer = 0;
+
+function chck () {
+    let question;
+    inpt = document.getElementById("inpt").value;
+    question_num = document.getElementById("num").innerHTML;
+    inpt = inpt.toUpperCase();
+    for (let i=0; i<=sets[question_num][0].length-1; i++) {
+        question = sets[question_num][0][i].toUpperCase();
+        if (inpt == question) {
+            document.getElementById("ans").style = "color:green"
+            document.getElementById("ans").innerHTML = "Correct"; 
+            console.log(`${inpt} == ${question}`);
+            if (wranswer == 0) {
+                done.push(question_num);
+            }
+            setTimeout(questGen, 2000);
+            return;
+
+        }
+    }
+    if (wranswer == 0) {
+        wranswer = 1;
+        document.getElementById("ans").style = "color:red"
+        document.getElementById("inpt").value = "";
+        document.getElementById("ans").innerHTML = `Incorrect, write it again<br>
+        ${sets[question_num][0][0][0].toUpperCase() + sets[question_num][0][0].substr(1)}`; 
+        console.log(`${inpt} != ${question}`);
+
+        // setTimeout(questGen, 5000);
+    }
+}
+
+function press () {
+    if (event.keyCode == 13) {
+        chck();
+    }
+}
+
+function inDone(num) {
+    for (let i=0; i<done.length; i++) {
+        if (done[i] == num) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function questGen () {
+    wranswer = 0;
+    document.getElementById("ans").innerHTML = "";
+    document.getElementById("ratio").innerHTML = `${done.length}/${sets.length}`;
+    document.getElementById("inpt").value = "";
+    let question_num;
+    do {
+        question_num = Math.floor(Math.random() * sets.length);
+    } while (inDone(question_num))
+    let tmpStr = "";
+
+    for (let i=0; i<sets[question_num][1].length; i++) {
+        if (i > 0) tmpStr += ", ";
+        tmpStr += sets[question_num][1][i][0].toUpperCase() + sets[question_num][1][i].substr(1);
+    }
+
+    document.getElementById("questionBox").innerHTML=tmpStr;   
+    document.getElementById("num").innerHTML=question_num;  
+}
